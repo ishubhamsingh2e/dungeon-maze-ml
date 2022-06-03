1e684b62850e558ef36d255b84b79e5c8e1416f6
shubham singh <ishubhamsingh2e@gmail.com>
2022-06-03 15:02:22 +0530

    base commit

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    private float speed = 1f;
+    private Vector3 moveDelta;
+    
+    void Start()
+    {
+        var boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        moveDelta = new Vector3(x,y,0);
+
+        if (moveDelta.x > 0){
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0){
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        transform.Translate(moveDelta * Time.deltaTime * speed);
+    }
+}
